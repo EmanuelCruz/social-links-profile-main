e460f38fd021d0c4a63dabb9880cf8187c55a360
Emanuel Cruz <emanuelcruz091@gmail.com>
2024-03-10 13:35:58 -0300

    library: add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
+  theme: {},
+  plugins: ["prettier-plugin-tailwindcss"],
+};
